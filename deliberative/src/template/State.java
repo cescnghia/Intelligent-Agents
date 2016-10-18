73eb4372c68fa2367a702e0b500400830bda6053
stephane cayssials <stephane.cayssials@epfl.ch>
2016-10-18 16:03:38 +0200

    update gitignore.

--- deliberative/src/template/State.java
+++ deliberative/src/template/State.java
@@ -1,5 +1,6 @@
 package template;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import logist.plan.Action;
@@ -19,20 +20,36 @@ public class State {
 	
 	private Vehicle mVehicle;
 	private TaskSet mAvailableTasks;
+	private TaskSet mCarriedTasks;
 	private City mCurrentCity;
 	private int mWeight;
 	private int mFreeWeight;
 	
-	public State(Vehicle mVehicule, TaskSet mAvailableTasks, City currentCity) {	
-		this.mVehicle = mVehicule;
-		this.mAvailableTasks = mAvailableTasks;
+	public State(Vehicle vehicule, TaskSet availableTasks, City currentCity) {	
+		this.mVehicle = vehicule;
+		this.mAvailableTasks = availableTasks;
 		this.mCurrentCity = currentCity;
 		
 		this.mWeight = 0;
-		for (Task task: mVehicle.getCurrentTasks()) {
+		this.mCarriedTasks = this.mVehicle.getCurrentTasks();
+		for (Task task: this.mCarriedTasks) {
 			this.mWeight += task.weight;
 		}
 		this.mFreeWeight = this.mVehicle.capacity() - this.mWeight;	
+	}
+	
+	public State(Vehicle vehicule, TaskSet availableTasks, City currentCity, int freeWeight, TaskSet carriedTasks) {
+		this.mVehicle = vehicule;
+		this.mAvailableTasks = availableTasks;
+		this.mCurrentCity = currentCity;
+		
+		this.mWeight = 0;
+		this.mCarriedTasks = carriedTasks;
+		for (Task task: this.mCarriedTasks) {
+			this.mWeight += task.weight;
+		}
+		this.mFreeWeight = this.mVehicle.capacity() - this.mWeight;
+		this.mFreeWeight = freeWeight;
 	}
 	
 	public boolean isFinal() {
@@ -42,6 +59,54 @@ public class State {
 			return true;
 		}
 		return false;
+	}
+	
+	public List<State> succ() {
+		List<State> stateList = new ArrayList<State>();
+		// fill the list with all the next state for each action type
+		// for PICKUP in a city or for DELIVERY in a city
+		for (ActionsEnum action: ActionsEnum.values()) {
+			if(action == ActionsEnum.PICKUP) {
+				// We check if one of more of all available task can be a next
+				// pickup state
+				for (Task taskAvailable : this.mAvailableTasks) {
+					// we check if the vehicle has enough place for the task
+					if (this.mFreeWeight < taskAvailable.weight) {
+						// stop here for this task because there is not enough place in the vehicle
+						continue;
+					}
+					// calculate the new freeWeight of the vehicle that has pickup the new task
+					int newFreeWeight = this.mFreeWeight + taskAvailable.weight;
+					// set the new city
+					City newCity = taskAvailable.pickupCity;
+					// set the new availableTasks Set by removing the picked one
+					TaskSet newAvailableTasks = TaskSet.copyOf(this.mAvailableTasks);
+					newAvailableTasks.remove(taskAvailable);
+					// set the new carriedTask Set
+					TaskSet newCarriedTasks = TaskSet.copyOf(this.mCarriedTasks);
+					newCarriedTasks.add(taskAvailable);
+					// add the new nextState to the nextState List
+					stateList.add(new State(this.mVehicle, newAvailableTasks, newCity, newFreeWeight, newCarriedTasks));
+				}
+			}
+			if (action == ActionsEnum.DELIVERY) {
+				// We check if one of more of all carriedTask can be Delivered
+				for (Task taskDelivrable : this.mCarriedTasks) {
+					// calculate the new freeWeight of the vehicle that has pickup the new task
+					int newFreeWeight = this.mFreeWeight - taskDelivrable.weight;
+					// set the new city
+					City newCity = taskDelivrable.deliveryCity;
+					// set the new availableTasks which is the same as previously
+					TaskSet newAvailableTasks = TaskSet.copyOf(this.mAvailableTasks);
+					// set the new carriedTask Set
+					TaskSet newCarriedTasks = TaskSet.copyOf(this.mCarriedTasks);
+					newCarriedTasks.remove(taskDelivrable);
+					// add the new nextState to the nextState List
+					stateList.add(new State(this.mVehicle, newAvailableTasks, newCity, newFreeWeight, newCarriedTasks));
+				}
+			}
+		}
+		return stateList;
 	}
 
 	public Vehicle getVehicule() {
