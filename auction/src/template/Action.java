84d21892c877f57e3f04a9ad10144833643a6d47
stephane cayssials <stephane.cayssials@epfl.ch>
2016-11-23 13:27:58 +0100

    no message

--- auction/src/template/Action.java
+++ auction/src/template/Action.java
@@ -1,5 +1,70 @@
 package template;
 
-public class Action {
+import logist.task.Task;
+import logist.topology.Topology.City;
 
+public abstract class Action {
+	
+  public final Task task;
+  public final City actionCity;
+  
+  public Action(Task task, City aCity) {
+    this.task = task;
+    this.actionCity = aCity;
+  }
+  
+  public boolean isPickup(){
+    return false;
+  }
+  
+  public boolean isDelivery(){
+    return false;
+  }
+  
+}
+
+class Pickup extends Action{
+
+  public Pickup(Task task) {
+    super(task, task.pickupCity);
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    return obj instanceof Pickup && ((Pickup)obj).task.equals(this.task);
+  }
+  
+  @Override
+  public boolean isPickup() {
+    return true;
+  }
+  
+  @Override
+  public String toString() {
+    return "Pickup("+task.pickupCity+")";
+  }
+  
+}
+
+class Deliver extends Action{
+
+  public Deliver(Task task) {
+    super(task, task.deliveryCity);
+  }
+  
+  @Override
+  public boolean equals(Object obj) {
+    return obj instanceof Deliver && ((Deliver)obj).task.equals(this.task);
+  }
+  
+  @Override
+  public boolean isDelivery() {
+    return true;
+  }
+  
+  @Override
+  public String toString() {
+    return "Deliver("+task.deliveryCity+")";
+  }
+  
 }
