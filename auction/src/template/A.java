405a6a056cfa838450861d5d983d6676e282e5c3
Anh-Nghia Khau <anh.khau@epfl.ch>
2016-11-25 14:58:59 +0100

    some main implementation

---
+++ auction/src/template/A.java
@@ -0,0 +1,107 @@
+package template;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import template.Task_.Action;
+import logist.simulation.Vehicle;
+ /**
+  * 
+  *  @author Anh Nghia Khau and Stephane Cayssials
+  *
+  *	Represent a plan for our problem. 
+  * We took the name "A" for the compatibility with the one in the slide for Stochastic Local Search Algorithm
+  *
+  */
+public class A {
+
+	private HashMap<Vehicle, LinkedList<Task_>> mVehicleTasks;
+	private List<Vehicle> mVehicles = new ArrayList<Vehicle>();
+	
+	public A(HashMap<Vehicle, LinkedList<Task_>> map){
+		this.mVehicleTasks = map;
+		this.mVehicles.addAll(map.keySet());
+	}
+	
+	public A(A that){
+		this.mVehicleTasks = new HashMap<Vehicle, LinkedList<Task_>>(that.mVehicleTasks);
+	}
+	
+	public A(ArrayList<Vehicle> vehicles){
+		for (Vehicle v : vehicles)
+			mVehicleTasks.put(v, new LinkedList<Task_>());
+		this.mVehicles = vehicles;
+	}
+	
+	public List<Vehicle> getVehicles() {return this.mVehicles ;}
+	
+	public HashMap<Vehicle, LinkedList<Task_>> getMap(){
+		return this.mVehicleTasks;
+	}
+	
+	public LinkedList<Task_> getTasksOfVehicle(Vehicle v){
+		return this.mVehicleTasks.get(v);
+	}
+	
+	/*------ALL TRANSFORMATIONS METHODS------*/
+	
+	// Add a task to the head
+	public void addTaskForVehicle(Task_ a, Vehicle v){
+		LinkedList<Task_> tasks = new LinkedList<Task_>(mVehicleTasks.get(v));
+		tasks.addFirst(a);
+		this.mVehicleTasks.put(v, tasks);
+	}
+	
+	// Remove a specific task 
+	public void removeTaskFromVehicle(Task_ a, Vehicle v){
+		LinkedList<Task_> tasks = new LinkedList<Task_>(mVehicleTasks.get(v));
+		tasks.remove(a);
+		this.mVehicleTasks.put(v, tasks);
+	}
+	
+	// Change order of 2 tasks
+	public void changeOrderOfTwoTasks(int idx1, int idx2, Vehicle v){
+		LinkedList<Task_> tasks = mVehicleTasks.get(v);
+
+		Task_ task1 = tasks.get(idx1);
+		Task_ task2 = tasks.get(idx2);
+		
+		tasks.set(idx1, task2);
+		tasks.set(idx2, task1);
+		this.mVehicleTasks.put(v, tasks);
+	}
+	
+	/*--------COMPUTE COST OF THIS PLAN---------*/
+	
+	public double cost(){
+		double cost = 0.0;
+		
+		for (Vehicle v : mVehicleTasks.keySet()){
+			
+			if (mVehicleTasks.get(v).isEmpty()) //no task for this vehicle => continue
+				continue;
+			
+			// Cost for going to pickup a task at the beginning
+			cost += (v.homeCity().distanceTo(mVehicleTasks.get(v).getFirst().getTask().pickupCity))*v.costPerKm();
+			LinkedList<Task_> tasks = this.mVehicleTasks.get(v);
+			
+			for (int i = 0 ; i < tasks.size() - 1 ; i++ ){
+				Task_ task = tasks.get(i);
+				Task_ nextTask = tasks.get(i+1);
+				
+				if (task.getAction() == Action.PICKUP && nextTask.getAction() == Action.PICKUP){
+					cost += task.getTask().pickupCity.distanceTo(nextTask.getTask().pickupCity)*v.costPerKm();
+				} else if (task.getAction() == Action.DELIVERY && nextTask.getAction() == Action.PICKUP){
+					cost += task.getTask().deliveryCity.distanceTo(nextTask.getTask().pickupCity)*v.costPerKm();
+				} else if (task.getAction() == Action.PICKUP && nextTask.getAction() == Action.DELIVERY){
+					cost += task.getTask().pickupCity.distanceTo(nextTask.getTask().deliveryCity)*v.costPerKm();
+				} else {
+					cost += task.getTask().deliveryCity.distanceTo(nextTask.getTask().deliveryCity)*v.costPerKm();
+				}
+			}
+		}	
+		return cost;
+	}
+}
--- auction/src/template/AuctionTemplate.java
+++ auction/src/template/AuctionTemplate.java
@@ -2,11 +2,15 @@ package template;
 
 //the list of imports
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
+import template.Task_.Action;
+import logist.LogistSettings;
 import logist.Measures;
 import logist.behavior.AuctionBehavior;
+import logist.config.Parsers;
 import logist.agent.Agent;
 import logist.simulation.Vehicle;
 import logist.plan.Plan;
@@ -30,11 +34,38 @@ public class AuctionTemplate implements AuctionBehavior {
 	private Random random;
 	private Vehicle vehicle;
 	private City currentCity;
+    private long timeout_setup;
+    private long timeout_plan;
+	
+	// Agent's parameters
+	private PickupDeliveryProblem myPDP;
+	
+	private double myBestCost;
+	private A myBestPlan;
+	
+	private A mBestOpponentPlan;
+	private double mBestOpponentCost;
+	private TaskSet mOpponentTaskSet;
 
 	@Override
 	public void setup(Topology topology, TaskDistribution distribution,
 			Agent agent) {
-
+		
+        // this code is used to get the timeouts
+        LogistSettings ls = null;
+        try {
+            ls = Parsers.parseSettings("config/settings_default.xml");
+        }
+        catch (Exception exc) {
+            System.out.println("There was a problem loading the configuration file.");
+            System.out.println(exc.getMessage());
+        }
+        
+        // the setup method cannot last more than timeout_setup milliseconds
+        timeout_setup = ls.get(LogistSettings.TimeoutKey.SETUP);
+        // the plan method cannot execute more than timeout_plan milliseconds
+        timeout_plan = ls.get(LogistSettings.TimeoutKey.PLAN);
+		
 		this.topology = topology;
 		this.distribution = distribution;
 		this.agent = agent;
@@ -43,21 +74,50 @@ public class AuctionTemplate implements AuctionBehavior {
 
 		long seed = -9019554669489983951L * currentCity.hashCode() * agent.id();
 		this.random = new Random(seed);
+		
+		ArrayList<Vehicle> myVehicles = new ArrayList<Vehicle>(agent.vehicles());
+		
+		this.myPDP = new PickupDeliveryProblem(myVehicles);
+		
+		
 	}
 
+	// There are only 2 agent (our agent and opponent agent)
+	// Check status of opponent
+	// Control the actually winner 
 	@Override
 	public void auctionResult(Task previous, int winner, Long[] bids) {
-		if (winner == agent.id()) {
+		
+		if (winner == agent.id()) { // We win this task
+			// we win for the task
+			// store the value of newCost and newPlan that we have computed in the method askPrice()
+			// continue the auction
 			currentCity = previous.deliveryCity;
+			
+		} else { // Do Something
+			// analyze the plan of opponent
+			// do some strategy....
 		}
 	}
 	
+	// function 
 	@Override
 	public Long askPrice(Task task) {
 
 		if (vehicle.capacity() < task.weight)
 			return null;
-
+		
+		/*------------TO DO------------*/
+		
+		// add this new task to the old plan
+		A newPlan = this.myPDP.addNewTask(task);
+		// compute new cost for this new plan
+		double newCost = newPlan.cost();
+		// return a bid in function of new cost
+		
+		
+		
+		// Code given by ASSITANT
 		long distanceTask = task.pickupCity.distanceUnitsTo(task.deliveryCity);
 		long distanceSum = distanceTask
 				+ currentCity.distanceUnitsTo(task.pickupCity);
@@ -69,41 +129,56 @@ public class AuctionTemplate implements AuctionBehavior {
 
 		return (long) Math.round(bid);
 	}
+	
+	
+	
+    @Override
+    public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
+        long time_start = System.currentTimeMillis();
 
-	@Override
-	public List<Plan> plan(List<Vehicle> vehicles, TaskSet tasks) {
+       // Plan is already created by the askPrice()
+        this.myBestPlan = this.myPDP.getBestA();
+        this.myBestCost = myBestPlan.cost();
+        
+        System.out.println("The minimun cost is: "+myBestCost);
+        
+        List<Vehicle> myVehicles = myBestPlan.getVehicles();
+        
+        List<Plan> plans = new ArrayList<Plan>();
+
+        for (Vehicle v : myVehicles){
+        	LinkedList<Task_> tasks_ = myBestPlan.getTasksOfVehicle(v);
+        	if (tasks_ != null) {
+        		Plan plan = makePlan(v, tasks_);
+       			plans.add(plan);
+        	}
+        }
+        
+        long time_end = System.currentTimeMillis();
+        long duration = time_end - time_start;
+        System.out.println("The plan was generated in "+duration+" milliseconds.");
+        
+        return plans;
+    }
+    
+    private Plan makePlan(Vehicle v, LinkedList<Task_> tasks) {
+		City currentCity = v.homeCity();
+		Plan plan = new Plan(currentCity);
 		
-//		System.out.println("Agent " + agent.id() + " has tasks " + tasks);
-
-		Plan planVehicle1 = naivePlan(vehicle, tasks);
-
-		List<Plan> plans = new ArrayList<Plan>();
-		plans.add(planVehicle1);
-		while (plans.size() < vehicles.size())
-			plans.add(Plan.EMPTY);
-
-		return plans;
-	}
-
-	private Plan naivePlan(Vehicle vehicle, TaskSet tasks) {
-		City current = vehicle.getCurrentCity();
-		Plan plan = new Plan(current);
-
-		for (Task task : tasks) {
-			// move: current city => pickup location
-			for (City city : current.pathTo(task.pickupCity))
-				plan.appendMove(city);
-
-			plan.appendPickup(task);
-
-			// move: pickup location => delivery location
-			for (City city : task.path())
-				plan.appendMove(city);
-
-			plan.appendDelivery(task);
-
-			// set current city
-			current = task.deliveryCity;
+		for(Task_ t : tasks){
+			if (t.getAction() == Action.PICKUP){
+				City nextCity = t.getTask().pickupCity;
+				for (City city : currentCity.pathTo(nextCity))
+					plan.appendMove(city);
+				currentCity = nextCity;
+				plan.appendPickup(t.getTask());
+			} else {
+				City nextCity = t.getTask().deliveryCity;
+				for (City city : currentCity.pathTo(nextCity))
+					plan.appendMove(city);
+				currentCity = nextCity;
+				plan.appendDelivery(t.getTask());
+			}
 		}
 		return plan;
 	}
