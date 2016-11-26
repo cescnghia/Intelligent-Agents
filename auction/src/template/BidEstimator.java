afe63dd045858df681efb87a3924e4eb2a829566
stephane cayssials <stephane.cayssials@epfl.ch>
2016-11-26 17:26:52 +0100

    add bid estimator

---
+++ auction/src/template/BidEstimator.java
@@ -0,0 +1,41 @@
+package template;
+
+public class BidEstimator {
+
+	// Agent's parameters
+	private PickupDeliveryProblem mBestPDP;
+	private double mBestCost = Double.MAX_VALUE;
+	private A mBestPlan = null;
+	
+	// Parameters intermedia
+	// Using to comptute new cost and new plan in askPrice() method
+	private PickupDeliveryProblem mNewPDP;
+	private double mNewCost = Double.MAX_VALUE;
+	private A mNewPlan = null;
+	
+	public BidEstimator(PickupDeliveryProblem bestPDP, PickupDeliveryProblem newPDP) {
+		this.mBestPDP = bestPDP.clone();
+		this.mNewPDP = newPDP.clone();
+	}
+	
+	/**
+	 * Get the bid
+	 * 
+	 * @return the bid
+	 */
+	public long getBid() {
+		long lowBound = this.getLowBound();
+		return lowBound;
+	}
+	
+	/**
+	 * Get the lower bound for the bid if we want to make money
+	 * difference between the bestPDP and the NewPDP with the new task
+	 * 
+	 * @return the lower bound for the bid
+	 */
+	private long getLowBound() {
+		return (long) (this.mNewPDP.getCost() - this.mBestPDP.getCost());
+	}
+	
+}
